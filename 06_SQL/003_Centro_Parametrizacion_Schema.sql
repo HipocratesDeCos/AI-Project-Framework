8c6a7ce776cf053f76afe3a1506ab1d3651e6003
HipocratesDeCos <andresragel2024@gmail.com>
2026-08-31 12:16:11 +0200

    Corregir FK del histórico del Centro de Parametrización

--- 06_SQL/003_Centro_Parametrizacion_Schema.sql
+++ 06_SQL/003_Centro_Parametrizacion_Schema.sql
@@ -66,9 +66,8 @@ BEGIN
         CONSTRAINT PK_parameter_configuration_history
             PRIMARY KEY CLUSTERED (history_id),
         CONSTRAINT FK_parameter_configuration_history_configuration
-            FOREIGN KEY (configuration_id, parameter_id, company_id)
-            REFERENCES eios.parameter_configuration
-                (configuration_id, parameter_id, company_id),
+            FOREIGN KEY (configuration_id)
+            REFERENCES eios.parameter_configuration (configuration_id),
         CONSTRAINT CK_parameter_configuration_history_parameter_nonempty
             CHECK (LEN(LTRIM(RTRIM(parameter_id))) > 0),
         CONSTRAINT CK_parameter_configuration_history_company_nonempty
