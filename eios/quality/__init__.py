86ed3e53c785e7e9d47f02e3bfd867e34f469998
HipocratesDeCos <andresragel2024@gmail.com>
2026-08-31 13:06:20 +0200

    Export Quality & Trust gate API

--- eios/quality/__init__.py
+++ eios/quality/__init__.py
@@ -1,1 +1,17 @@
 """EIOS Quality & Trust components."""
+
+from .gate import (
+    QualityCheck,
+    QualityConfidence,
+    QualityStatus,
+    QualityTrustResult,
+    evaluate_quality,
+)
+
+__all__ = [
+    "QualityCheck",
+    "QualityConfidence",
+    "QualityStatus",
+    "QualityTrustResult",
+    "evaluate_quality",
+]
