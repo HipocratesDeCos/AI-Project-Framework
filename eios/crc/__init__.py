3449a15a5ea12b4ed2f5a4ab5126c97d2f2af37b
HipocratesDeCos <andresragel2024@gmail.com>
2026-09-01 20:06:36 +0200

    revert(crc): remove duplicate CRC implementation

--- eios/crc/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CRC-MVP deterministic consolidation."""
-
-from .engine import consolidate_crc
-from .models import CRCConflict, CRCInput, CRCResult, CRCRuleMetadata
-
-__all__ = ["CRCConflict", "CRCInput", "CRCResult", "CRCRuleMetadata", "consolidate_crc"]
