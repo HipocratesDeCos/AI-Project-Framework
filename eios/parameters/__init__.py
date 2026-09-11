967e6ec8c73490a39b57a916eb7f3850616ef49e
HipocratesDeCos <andresragel2024@gmail.com>
2026-09-11 18:50:04 +0200

    feat(parameters): export contextual configuration resolution

--- eios/parameters/__init__.py
+++ eios/parameters/__init__.py
@@ -1,1 +1,5 @@
 """EIOS parameter-configuration components."""
+
+from .resolution import ResolvedConfiguration, resolve_configuration_for_context
+
+__all__ = ["ResolvedConfiguration", "resolve_configuration_for_context"]
