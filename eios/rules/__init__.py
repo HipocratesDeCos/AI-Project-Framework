b9d5f5110b7338d2b36b5a359d0d187d3b0c9519
HipocratesDeCos <andresragel2024@gmail.com>
2026-09-11 18:29:03 +0200

    feat(rules): expose multi-rule vertical runtime

--- eios/rules/__init__.py
+++ eios/rules/__init__.py
@@ -7,7 +7,14 @@ from .delivery import (
     evaluate_r_ent_001,
 )
 from .delivery_runtime import REnt001VerticalResult, run_r_ent_001_vertical
-from .runtime import ConsolidatedBaseResult, RuleVerticalResult, run_assessment_vertical
+from .runtime import (
+    ConsolidatedBaseResult,
+    RuleAssessmentBinding,
+    RuleSetVerticalResult,
+    RuleVerticalResult,
+    run_assessment_set_vertical,
+    run_assessment_vertical,
+)
 
 __all__ = [
     "BASELINE_EVIDENCE_SOURCE_TYPE",
@@ -15,8 +22,11 @@ __all__ = [
     "DELIVERY_EVIDENCE_SOURCE_TYPE",
     "R_ENT_001",
     "REnt001VerticalResult",
+    "RuleAssessmentBinding",
+    "RuleSetVerticalResult",
     "RuleVerticalResult",
     "evaluate_r_ent_001",
+    "run_assessment_set_vertical",
     "run_assessment_vertical",
     "run_r_ent_001_vertical",
 ]
