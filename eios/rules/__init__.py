f387c12f2280acdf269cda75d4689ab68ecb303b
HipocratesDeCos <andresragel2024@gmail.com>
2026-09-11 19:53:43 +0200

    feat(rules): expose E2E C0 invoker adapter

--- eios/rules/__init__.py
+++ eios/rules/__init__.py
@@ -9,6 +9,7 @@ from .delivery import (
 )
 from .delivery_runtime import REnt001VerticalResult, run_r_ent_001_vertical
 from .engine import RulesEngineInput, RulesEngineResult, run_rules_engine
+from .execution_adapter import RulesEngineC0Invoker, build_rules_engine_c0_invoker
 from .finance import (
     FINANCE_BASIC_EVIDENCE_SOURCE_TYPE,
     PARAMETER_CONFIGURATION_EVIDENCE_SOURCE_TYPE,
@@ -66,6 +67,7 @@ __all__ = [
     "RuleAssessmentBinding",
     "RuleSetVerticalResult",
     "RuleVerticalResult",
+    "RulesEngineC0Invoker",
     "RulesEngineInput",
     "RulesEngineResult",
     "STOCK_CONFIRMED_DEMAND_EVIDENCE_SOURCE_TYPE",
@@ -73,6 +75,7 @@ __all__ = [
     "authorized_rule",
     "authorized_rule_metadata",
     "bind_authorized_assessment",
+    "build_rules_engine_c0_invoker",
     "evaluate_r_ent_001",
     "evaluate_r_fin_001",
     "evaluate_r_fin_003",
