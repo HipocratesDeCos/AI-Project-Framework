1016cb28ef0460b76d5c912a7a6d728baf7be566
HipocratesDeCos <andresragel2024@gmail.com>
2026-09-11 18:51:02 +0200

    feat(fin): export R-FIN-001 bridge

--- eios/rules/__init__.py
+++ eios/rules/__init__.py
@@ -7,6 +7,14 @@ from .delivery import (
     evaluate_r_ent_001,
 )
 from .delivery_runtime import REnt001VerticalResult, run_r_ent_001_vertical
+from .finance import (
+    FINANCE_BASIC_EVIDENCE_SOURCE_TYPE,
+    PARAMETER_CONFIGURATION_EVIDENCE_SOURCE_TYPE,
+    P_FIN_002,
+    R_FIN_001,
+    evaluate_r_fin_001,
+    finance_basic_result_ref,
+)
 from .runtime import (
     ConsolidatedBaseResult,
     RuleAssessmentBinding,
@@ -28,7 +36,11 @@ __all__ = [
     "BASELINE_EVIDENCE_SOURCE_TYPE",
     "ConsolidatedBaseResult",
     "DELIVERY_EVIDENCE_SOURCE_TYPE",
+    "FINANCE_BASIC_EVIDENCE_SOURCE_TYPE",
+    "PARAMETER_CONFIGURATION_EVIDENCE_SOURCE_TYPE",
+    "P_FIN_002",
     "R_ENT_001",
+    "R_FIN_001",
     "R_STK_003",
     "R_STK_004",
     "REnt001VerticalResult",
@@ -38,8 +50,10 @@ __all__ = [
     "STOCK_CONFIRMED_DEMAND_EVIDENCE_SOURCE_TYPE",
     "STOCK_EXCESS_EVIDENCE_SOURCE_TYPE",
     "evaluate_r_ent_001",
+    "evaluate_r_fin_001",
     "evaluate_r_stk_003",
     "evaluate_r_stk_004",
+    "finance_basic_result_ref",
     "run_assessment_set_vertical",
     "run_assessment_vertical",
     "run_r_ent_001_vertical",
