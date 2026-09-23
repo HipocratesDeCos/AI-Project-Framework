bde74e3b8ef58f830f7857f613bafc6bf2d93249
HipocratesDeCos <andresragel2024@gmail.com>
2026-09-23 16:57:53 +0200

    feat(shadow): add assurance package

---
+++ eios/assurance/__init__.py
@@ -0,0 +1,4 @@
+"""EIOS Assurance capabilities."""
+from .shadow_mode import ObservedHumanDecision, ShadowModeResult, ShadowObservation, produce_shadow_mode_result
+
+__all__ = ["ObservedHumanDecision","ShadowModeResult","ShadowObservation","produce_shadow_mode_result"]
